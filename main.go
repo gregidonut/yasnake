1d87b39edeee5732c2beee6557c5f1b80d5386aa
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-02-16 07:21:33 +0800

    feat: add intial implemention of a moving snake
    this just moves upward since the main point is to apply a continious
    redraw the screen every game loop

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"image/color"
+	"time"
 )
 
 func main() {
@@ -13,13 +14,25 @@ func main() {
 	a := app.New()
 
 	w := a.NewWindow("YASnake")
-
-	w.SetContent(setupGame())
 	w.Resize(fyne.NewSize(200, 200))
 	w.SetFixedSize(true)
 
+	game = setupGame()
+	w.SetContent(game)
+
+	go runGame()
+
 	w.ShowAndRun()
 }
+
+type snakePart struct {
+	x, y float32
+}
+
+var (
+	snakeParts []snakePart
+	game       *fyne.Container
+)
 
 // setupGame() will be the starting position for the snake
 // since it defines how everything looks before redrawing the screen
@@ -36,7 +49,37 @@ func setupGame() *fyne.Container {
 		r.Move(fyne.NewPos(90, float32(50+i*10)))
 
 		segments = append(segments, r)
+
+		seg := snakePart{9, float32(5 + i)}
+		snakeParts = append(snakeParts, seg)
 	}
 
 	return container.NewWithoutLayout(segments...)
+}
+
+// refreshGame() is how the ui redraws the screen
+// which is by multiplying the units in snakePart's coordinate value
+// by 10, which will be the new position of the objects(snake segment)
+func refreshGame() {
+	for i, seg := range snakeParts {
+		rect := game.Objects[i]
+		rect.Move(fyne.NewPos(seg.x*10, seg.y*10))
+	}
+
+	game.Refresh()
+}
+
+// runGame is the main game loop that redraws the whole ui every
+// time.Tick() duration, by calling refreshGame() after changing
+// the y coordinate value (deducting it, causing the snake to go up
+// since the new position every snake segment is being assigned to
+// is decreasing
+func runGame() {
+	for range time.Tick(time.Millisecond * 500) {
+		for i := len(snakeParts) - 1; i >= 1; i-- {
+			snakeParts[i] = snakeParts[i-1]
+		}
+		snakeParts[0].y--
+		refreshGame()
+	}
 }
