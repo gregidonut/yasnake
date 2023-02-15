1d36b8bd2555555dcbfd053134dac8a9456defb0
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-02-16 05:15:59 +0800

    feat: add initial implemention of a static snake
    just as a starting point of how the window will render before it starts
    animating

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"image/color"
+)
+
+func main() {
+
+	a := app.New()
+
+	w := a.NewWindow("YASnake")
+
+	w.SetContent(setupGame())
+	w.Resize(fyne.NewSize(200, 200))
+	w.SetFixedSize(true)
+
+	w.ShowAndRun()
+}
+
+func setupGame() *fyne.Container {
+	var segments []fyne.CanvasObject
+
+	// draw snake by adding 10 rectangle elements to segments slice
+	for i := 0; i < 10; i++ {
+		r := canvas.NewRectangle(&color.RGBA{G: 0x66, A: 0xff})
+		r.Resize(fyne.NewSize(10, 10))
+
+		// use iteration variable to compute where the next rectangle will be
+		r.Move(fyne.NewPos(90, float32(50+i*10)))
+
+		segments = append(segments, r)
+	}
+
+	return container.NewWithoutLayout(segments...)
+}
